7f0f856877c697c1f72c88b084e38d7b27ebe137
kiyoshigawa <tim@twa.ninja>
2025-02-03 20:04:47 -0700

    Added DC801 Controller Board Config Files

---
+++ configs/DC801ControllerBadge/DC801ControllerBadge.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+// -----------------------------------------------------
+// NOTE: THIS HEADER IS ALSO INCLUDED BY ASSEMBLER SO
+//       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
+// -----------------------------------------------------
+
+
+#ifndef _BOARDS_DC801CONTROLLERBADGE_H
+#define _BOARDS_DC801CONTROLLERBADGE_H
+
+// For board detection
+#define DC801CONTROLLERBADGE
+
+// --- LED ---
+#ifndef PICO_DEFAULT_LED_PIN
+#define PICO_DEFAULT_LED_PIN 25
+#endif
+// no PICO_DEFAULT_WS2812_PIN
+
+// --- FLASH ---
+#define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
+
+#ifndef PICO_FLASH_SPI_CLKDIV
+#define PICO_FLASH_SPI_CLKDIV 2
+#endif
+
+#ifndef PICO_FLASH_SIZE_BYTES
+#define PICO_FLASH_SIZE_BYTES (32 * 1024 * 1024)
+#endif
+
+
+#ifndef PICO_RP2040_B0_SUPPORTED
+#define PICO_RP2040_B0_SUPPORTED 1
+#endif
+
+#endif
---
+++ configs/DC801ControllerBadge/README.md
@@ -0,0 +1,3 @@
+# GP2040 Configuration for the DC801 Controller Badge
+
+Basic pin setup for the DC801 Controller Badge
